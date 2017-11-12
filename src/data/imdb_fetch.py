90e9dc00398086015c341bd2242a06b658af5572
yOoMarvin <marvin.messenzehl@gmail.com>
2017-11-12 21:48:07 +0100

    add script and pseudocode in comments

---
+++ src/data/imdb_fetch.py
@@ -0,0 +1,10 @@
+# Script for fetching the imdb api data for budget and revenue for each movie in our dataset
+
+# 1. Import raw dataset
+# 2. Iterate through df
+# 3. Save the imdb id
+# 4. API call with that id
+# 5. Parse the json for revenue and budget
+# 6. Maybe process them if needed
+# 7. If there are values, write them in the budget and revenue column of the df
+# 8. If not, do nothing
