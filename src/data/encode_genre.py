f37522240e2a42059d5047f366a6c8100fc6e701
Adrian Kochsiek <adrian.kochsiek@outlook.de>
2017-11-12 12:04:53 +0100

    deleted prints

--- src/data/encode_genre.py
+++ src/data/encode_genre.py
@@ -14,9 +14,7 @@ def encodeGenre(df):
         new_value = re.sub("{(\'id\': \d+, )*\'name\': ", "", row["genres"])
         new_value = new_value.replace("[", "").replace("]", "").replace("\'", "").replace("}", "").replace(", ", "|")
         new_values.append(new_value)
-        print(new_value)
     new_values_encoded = pd.Series(new_values).str.get_dummies()
-    print(new_values_encoded.head())
     return new_values_encoded
 
 def encodeGenreToOne(df):
@@ -31,5 +29,4 @@ def encodeGenreToOne(df):
         new_value = new_value.replace("[", "").replace("]", "").replace("\'", "").replace("}", "").split(", ")[0]
         new_values.append(new_value)
     new_values_encoded = pd.get_dummies(new_values)
-    print(new_values_encoded.head())
     return new_values_encoded
