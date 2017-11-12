71400567e3446beeb45361c68e7f7206b7b44197
yOoMarvin <marvin.messenzehl@gmail.com>
2017-11-12 18:07:07 +0100

    Add binning and encoding with 3 categories

--- src/data/productivity.py
+++ src/data/productivity.py
@@ -9,8 +9,26 @@ data.head()
 
 data['productivity'] = data['revenue'] / data['budget']
 
+# Playground: binning
+data_binned = pd.DataFrame(dict(
+    productivity = pd.cut(data['productivity'], bins=3, labels=['low', 'middle', 'high'])
+))
+data_binned_and_encoded = pd.get_dummies(data_binned)
+data_binned_and_encoded
+
+
+
+
+
+
+
 
 ## Function for adding the new column
 def productivity_column(df):
     df['productivity'] = df['revenue'] / df['budget']
-    return df
+    # Binning and one hot encoding
+    df_binned = pd.DataFrame(dict(
+        productivity = pd.cut(df['productivity'], bins=3, labels=['low', 'middle', 'high'])
+    ))
+    df_binned_and_encoded = pd.get_dummies(df_binned)
+    return df_binned_and_encoded
