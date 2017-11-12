b10ac581ffa5ca7f82547b3f5b221f18c7fdf669
Adrian Kochsiek <adrian.kochsiek@outlook.de>
2017-11-12 10:54:40 +0100

    encode production company

--- src/data/encode_genre.py
+++ src/data/encode_genre.py
@@ -1,18 +1,15 @@
 import pandas as pd
-import json
 import re
 
 def encodeGenre():
-    #movies = pd.read_csv("../../data/interim/only_useful_datasets.csv")
-    movies = pd.read_csv("../../data/raw/movies_metadata.csv")
+    movies = pd.read_csv("../../data/interim/only_useful_datasets.csv")
     new_values = []
     for index, row in movies.iterrows():
-        new_row = row["genres"].replace("[", "").replace("]", "").replace("'", "\"")
-        newer = re.sub("{\"id\": \d+, \"name\": ", "", new_row)
-        newer = newer.replace("\"", "").replace("}", "").split(", ")[0]
-        new_values.append(newer)
-        #print(newer)
-    movies["genres"] = new_values
-    print(movies.head())
+        one_genre = re.sub("{(\'id\': \d+, )*\'name\': ", "", row["genres"])
+        one_genre = one_genre.replace("[", "").replace("]", "").replace("\'", "").replace("}", "").split(", ")[0]
+        new_values.append(one_genre)
+    new_values_encoded = pd.get_dummies(new_values)
+    print(new_values_encoded.head())
+    return new_values_encoded
 
 encodeGenre();
---
+++ src/data/encode_production_company.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import re
+
+def encodeProductionCompany():
+    movies = pd.read_csv("../../data/interim/only_useful_datasets.csv")
+    new_values = []
+    for index, row in movies.iterrows():
+        new_value = row["production_companies"].replace("[", "").replace("]", "").replace("{\'name\': ", "").replace("}", "").replace("\'", "")
+        new_value = re.sub(", id: \d+", "", new_value)
+        new_value = new_value.replace(", ", "|")
+        new_values.append(new_value)
+    new_values_encoded = pd.Series(new_values).str.get_dummies()
+    return new_values_encoded
+
+def encodeProductionCopmpanyToOne():
+    movies = pd.read_csv("../../data/interim/only_useful_datasets.csv")
+    new_values = []
+    for index, row in movies.iterrows():
+        new_value = row["production_companies"].replace("[", "").replace("]", "").replace("{\'name\': ", "").replace(
+            "}", "").replace("\'", "")
+        new_value = re.sub(", id: \d+", "", new_value)
+        new_value = new_value.split(", ")[0]
+        new_values.append(new_value)
+    new_values_encoded = pd.get_dummies(new_values)
+    return new_values_encoded
+
+encodeProductionCopmpanyToOne()
