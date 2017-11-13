3c77bd151d9a695b02d9bf1ddaf825403302493b
yOoMarvin <marvin.messenzehl@gmail.com>
2017-11-13 07:06:33 +0100

    Add exception handling and for loop

--- src/data/imdb_fetch.py
+++ src/data/imdb_fetch.py
@@ -8,3 +8,36 @@
 # 6. Maybe process them if needed
 # 7. If there are values, write them in the budget and revenue column of the df
 # 8. If not, do nothing
+
+
+import pandas as pd
+import numpy as np
+import requests
+import math
+
+df = pd.read_csv('../../data/raw/movies_metadata.csv')
+#remove NaN's from imdb_id column
+df = df[pd.notnull(df['imdb_id'])]
+#remove rows where imdb_id is 0
+df = df[df.imdb_id != '0']
+
+df.sort_values(by='imdb_id', ascending=False)
+
+
+
+base_url = "https://theimdbapi.org/api/movie?movie_id="
+
+
+for index, row in df.iterrows():
+    imdb_id = row['imdb_id']
+    if imdb_id != imdb_id:
+        url = base_url + imdb_id
+        print(url)
+
+
+
+r = requests.get('https://theimdbapi.org/api/movie?movie_id=tt0114709')
+
+
+j = r.json()
+j['metadata']['gross']
