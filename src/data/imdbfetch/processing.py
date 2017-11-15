16774f8125d6405896bb24b19ec5bd5c006feb34
yOoMarvin <marvin.messenzehl@gmail.com>
2017-11-15 06:18:11 +0100

    Added data processing and appending to dataframe

--- src/data/imdbfetch/processing.py
+++ src/data/imdbfetch/processing.py
@@ -5,14 +5,31 @@ import pandas as pd
 import json
 from pprint import pprint
 
+# Create empty dataframe for the results
+result = pd.DataFrame(columns=['imdb_id', 'revenue', 'budget'])
 
 
+# Open file and read from it
 with open('output.json', encoding='utf-8') as data_file:
    data = json.loads(data_file.read())
 
-pprint(data)
 
+# Iterate over idx and item of the list
+# Fetch the data
+for i, item in enumerate(data):
+    revenue = process_json(item['metadata']['gross'])
+    budget = process_json(item['metadata']['budget'])
+    imdb_id = item['imdb_id']
 
+    # If revenue and budget are not 0 append it to the result df
+    if revenue != 0 and budget != 0:
+        # Creating a DataFrame to append
+        result = result.append(pd.DataFrame({'imdb_id': imdb_id,
+                                            'revenue': revenue,
+                                            'budget': budget},
+                                             index=[i]))
+
+result.head()
 
 
 
