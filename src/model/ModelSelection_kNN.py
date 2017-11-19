9390e9e37aa889c79d236cf2f94f8e4fd5823fa6
steffen-jung <33086982+steffen-jung@users.noreply.github.com>
2017-11-19 16:17:23 +0100

    Model selection for test with kNN

--- src/model/ModelSelection_kNN.py
+++ src/model/ModelSelection_kNN.py
@@ -8,21 +8,22 @@ Created on Thu Nov 16 13:09:06 2017
 import ClassifierTemplate as ct
 import pandas as pd
 
-data = {
-        'col1':[0.36,0.86,0.17,0.58,0.3,0.53,0.15,0.11,0.98,0.49,0.54,0.21,0.15,0.11,0.08,0.81,0.87,0.24,0.39,0.89],
-        'col2':[0.76,0.83,0.54,0.52,0.05,0.6,0.21,0.29,0.63,0.72,0.94,0.08,0.41,0.18,0.4,0.76,0.2,0.08,0.44,0.55],
-        'col3':[0.48,0.22,0.51,0.45,0.29,0.49,0.61,0.82,0.81,0.28,0.63,0.1,0.53,0.54,0.03,0.14,0.06,0.76,0.38,0.15],
-        'col_label':[0,1,2,0,1,2,0,1,2,0,1,2,0,1,2,0,1,2,0,1]
-        }
+data = pd.read_csv("../../data/processed/train_set.csv", index_col=0)
 
 # DataFrame containing label (!)
 df = pd.DataFrame(data)
 
 # Build Classifier object with DataFrame and column name of truth values
-c = ct.Classifier(df,'col_label')
+c = ct.Classifier(df,"productivity_binned")
+
+# drop columns not needed for Classification
+c.dropColumns([
+        "original_title",
+        "id.1"
+])
 
 # get parameters for GridSearch
-scorer = c.f1(average='micro') # use F1 score with micro averaging
+scorer = c.f1(average="micro") # use F1 score with micro averaging
 estimator = c.knn() # get kNN estimator
 cv = c.fold(
         k=2
