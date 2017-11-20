5b05b367c16d2bbdd45d4acafa404154b93226d1
steffen-jung <33086982+steffen-jung@users.noreply.github.com>
2017-11-20 19:38:14 +0100

    cleaning of company names and encoding enhanced

--- src/data/encode_production_company.py
+++ src/data/encode_production_company.py
@@ -2,6 +2,9 @@ import pandas as pd
 import re
 from addPrefixToColumn import addPrefixToColumn
 
+import extract_companies as ec
+import clean_companies as cc
+
 def encodeProductionCompany(df, filter=False, threshold=0.0005):
     """
     MultipleHotEncode column production_companies
@@ -13,7 +16,21 @@ def encodeProductionCompany(df, filter=False, threshold=0.0005):
     :param threshold: threshold for filter
     :return: encoded DataFrame
     """
+    
+    ec.extract_companies(df.index.values)
+    cc.clean_companies()
+    
+    m2c = pd.read_csv("../../data/interim/companies_to_movies.csv", index_col=0)
+    clean = pd.read_csv("../../data/interim/companies_cleaned.csv", index_col=0)
+    
+    joined = m2c.join(clean, on="company")
+    joined.drop("company", axis=1, inplace = True)
+    joined.drop_duplicates(["movie","name"], inplace=True)
+    
+    piv = pd.pivot_table(joined,index=["movie"],values=["name"],aggfunc=lambda x: '#'.join(x))
+    new_values_encoded = piv["name"].str.get_dummies(sep="#")
 
+    """
     new_values = []
     indices = []
     for index, row in df.iterrows():
@@ -23,10 +40,14 @@ def encodeProductionCompany(df, filter=False, threshold=0.0005):
         new_values.append(new_value)
         indices.append(index)
     new_values_encoded = pd.Series(new_values,index=indices).str.get_dummies()
-    if(filter):
-        new_values_encoded = filterWithThreshold(new_values_encoded, threshold)
+    """
+    
+    
+    if (filter):    
+       new_values_encoded = filterWithThreshold(new_values_encoded, threshold)
     new_values_encoded = addPrefixToColumn(new_values_encoded, "company")
     return new_values_encoded
+
 
 
 def encodeProductionCompanyToOne(df, filter=False, threshold=0.0005):
