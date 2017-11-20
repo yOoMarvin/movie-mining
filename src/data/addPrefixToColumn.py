dd834f2d7ed9a83eec5d24d329acf9f94c06d680
mkoller17 <martin@mkoller.eu>
2017-11-21 00:00:03 +0100

    improvements encode companies

--- src/data/addPrefixToColumn.py
+++ src/data/addPrefixToColumn.py
@@ -5,7 +5,11 @@ def addPrefixToColumn(df, prefix):
     :param prefix: prefix to add to the column name
     :return: DataFrame with changed columnnames
     """
+    i = 0
+
     for column in df:
         if(column != "id:"):
             df = df.rename(columns={column: prefix+"_"+str(column)})
+            i = i + 1
+            print(i)
     return df
--- src/data/preprocess_data.py
+++ src/data/preprocess_data.py
@@ -15,45 +15,50 @@ import encode_actors as ea
 # read in raw csv files
 metadata = pd.read_csv("../../data/raw/movies_metadata.csv", index_col=5)
 
+status = '[Status: ]'
 
 #limit metadata to relevant columns and rows only
 metadata = ic.interesting_columns(metadata)
-print('limited to interesting columns')
+print(status + 'limited to interesting columns')
 actors = pd.read_csv("../../data/raw/credits.csv", index_col=2)
 metadata = pd.merge(metadata, actors, left_index=True, right_index=True)
 
 # metadata: convert collection to boolean 
 metadata = cc.collection_to_boolean(metadata)
-print('collection is converted to boolean')
+print(status + 'collection is converted to boolean')
 
 # metadata: convert year + encode quarter 
 metadata = cr.years_quarters(metadata)
 metadata = eq.quarter_encoding(metadata)
-print('year converted, quarter encoded')
+print(status + 'year converted, quarter encoded')
 
 # metadata: encode company. country, genre and attach to dataframe. This is not done by the method itself
 metadata = pd.concat([metadata, ep_country.encodeProductionCountry(metadata)], axis=1)
-print('encoded country')
+print(status + 'encoded country')
 metadata = pd.concat([metadata, eg.encodeGenre(metadata)], axis=1)
-print('encoded genre')
+print(status + 'encoded genre')
 metadata = pd.concat([metadata, p.productivity_column(metadata)], axis=1)
-print('encoded productivity')
+print(status + 'encoded productivity')
+#print(epc.encodeProductionCompany(metadata))
 metadata = pd.concat([metadata, epc.encodeProductionCompany(metadata)], axis=1)
-print('encoded company')
+#print(encodedCompanies.keys())
+print(status + 'encoded company')
 
 # metadata: normalize data here if necessary. Input: df and string, Output: completed dataframe with normalized column Example: runtime
 metadata = nc.normalize_column_data(metadata, 'runtime')
 metadata = nc.normalize_column_data(metadata, 'year')
-print('data normalized')
+print(status + 'data normalized')
+
 
 #process actor column (returned)
-
 actors_column_processed = ea.encodeActorsToOne(metadata);
 actors_column_processed = actors_column_processed.reset_index()
 actors_column_processed = actors_column_processed.set_index(metadata.index)
+#print(actors_column_processed.keys())
+# print(metadata.head())
 
 # metadata: merge again with metadata
-metadata = pd.concat([metadata, actors], axis=1)
+metadata = pd.concat([metadata, actors_column_processed], axis=1)
 
 # metadata: drop irrelevant data
 metadata = metadata.drop([
@@ -66,9 +71,9 @@ metadata = metadata.drop([
         ,'quarter'
         ,'productivity'
 ],1)
-print('dropped irrelevant data')
+print(status + 'dropped irrelevant data')
 
-
+print(metadata.keys())
 #safe dataset to file, important: encode as UTF-8
 metadata.to_csv("../../data/interim/only_useful_datasets.csv", encoding='utf-8')
 print('new dataset should be saved, doublcheck in folder')
