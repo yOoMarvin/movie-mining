dd27b34cca81df8301cf6405e7ce2fb8e891f2ee
Adrian Kochsiek <adrian.kochsiek@outlook.de>
2017-11-20 21:05:33 +0100

    added support vector classification

---
+++ src/model/ModelSelection_svc.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+
+import ClassifierTemplate as ct
+import pandas as pd
+
+data = pd.read_csv("../../data/processed/train_set.csv", index_col=0)
+
+# DataFrame containing label (!)
+df = pd.DataFrame(data)
+
+# Build Classifier object with DataFrame and column name of truth values
+c = ct.Classifier(df,"productivity_binned")
+
+# drop columns not needed for Classification
+c.dropColumns([
+        "original_title",
+        "id.1",
+        "productivity"
+])
+
+# get parameters for GridSearch
+scorer = c.f1(average="micro") # use F1 score with micro averaging
+estimator = c.svc() # get svc estimator
+cv = c.fold(
+        k=10
+        ,random_state=42
+) # KStratifiedFold with random_state = 42
+# parameters to iterate in GridSearch
+parameters = {
+    'multi_class':['ovr', 'crammer_singer'],
+    # parameter can be used to tweak parallel computation / n = # of jobs
+    #,"n_jobs":[1]
+}
+
+
+# compute GridSearch
+gs = c.gridSearch(
+        estimator
+        ,scorer
+        ,parameters
+        ,print_results=False # let verbose print the results
+        ,verbose=2
+        ,cv=cv
+)
+
+# print best result
+c.gridSearchBestScore(gs)
+
+# save all results in csv
+c.gridSearchResults2CSV(gs,parameters,"svc_results.csv")
