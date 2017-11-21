fd80e18e1766beee846cf6ab35ba29a8070c06c3
yOoMarvin <marvin.messenzehl@gmail.com>
2017-11-21 21:57:57 +0100

    Change bin values and exploring structure to explore the results

--- src/playground/exploring.py
+++ src/playground/exploring.py
@@ -2,36 +2,15 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy
 
-data = pd.read_csv('../../data/interim/only_useful_datasets.csv')
-data.head()
-
-data['productivity'] = data['revenue'] / data['budget']
+prd = pd.read_csv('../../data/processed/productivity.csv')
+prd.head()
 
 
-
-data_filtered = data[['original_title', 'budget','revenue', 'release_date', 'productivity']]
-data_filtered[ data_filtered['revenue']<100 ].sort_values(by='revenue', ascending=False)
-
-
-data_filtered
-
-data_filtered['budget'].describe()
-
-plt.figure(figsize=(10,8))
-data_filtered['revenue'].plot()
+from collections import Counter
+counts = Counter(prd['productivity_binned'])
+df = pd.DataFrame.from_dict(counts, orient='index')
+df.plot(kind='bar')
 plt.show()
 
-
-
-# Kill outliers
-arr = data_filtered['productivity']
-
-elements = numpy.array(arr)
-
-mean = numpy.mean(elements, axis=0)
-sd = numpy.std(elements, axis=0)
-
-final_list = [x for x in arr if (x > mean - 0.5 * sd)]
-final_list = [x for x in final_list if (x < mean + 0.5 * sd)]
-final_list
-len(final_list)
+counts
+prd.sort_values(by='productivity', ascending=False)
