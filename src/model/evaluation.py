177759c5bca909612d2a6166ec6929463cc0ae1d
mkoller17 <martin@mkoller.eu>
2017-11-22 08:18:47 +0100

    changes for evaluation

--- src/model/evaluation.py
+++ src/model/evaluation.py
@@ -7,39 +7,23 @@ Created on Thu Nov 16 13:07:06 2017
 from sklearn import preprocessing
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import csv
-
-from sklearn.model_selection import train_test_split
-from sklearn.model_selection import GridSearchCV
-
-from sklearn.metrics import f1_score
-from sklearn.metrics import make_scorer
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import classification_report
-
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.naive_bayes import GaussianNB
-from sklearn import tree
-from sklearn.neighbors import NearestCentroid
-
 from sklearn.model_selection import StratifiedKFold
-from sklearn.model_selection import cross_val_predict
 from sklearn.model_selection import cross_val_score
-from sklearn import metrics
+from sklearn.preprocessing import LabelEncoder
 
 
 # Custom imports
 # Do not import all classifiers, rather look for the best result and fit them manually
-import ClassifierTemplate as ct
+
 
 
 
 # Import data
-data = pd.read_csv("../../data/interim/only_useful_datasets.csv", index_col=0)
-
+df = pd.read_csv("../../data/interim/only_useful_datasets.csv", index_col=0)
+print('data imported')
 # DataFrame containing label (!)
-df = pd.DataFrame(data)
+#df = pd.DataFrame(data)
 
 # Target
 target = df['productivity_binned']
@@ -47,7 +31,8 @@ target = df['productivity_binned']
 # Drop unwanted columns
 columns_to_drop = [
         "original_title",
-        "id.1"
+        "productivity_binned",
+        "id"
 ]
 df = df.drop(columns_to_drop, axis=1)
 
@@ -69,13 +54,18 @@ knn_estimator = KNeighborsClassifier(n_neighbors=3,
                             metric="manhattan")
 
 #ERROR HERE
+#print(target.values)
+#lb = LabelEncoder()
+#target = lb.fit_transform(target)
+#print(target)
 knn_estimator.fit(df, target)
+print('done doing the fit')
 
 # Predict with cross validation
-knn_scores = cross_val_score(knn_estimator, df, target, cv=cv, scoring='f1')
+knn_scores = cross_val_score(knn_estimator, df, target, cv=cv, scoring='f1_micro')
 
 
-print("KNN F1 Score: %0.2f (+/- %0.2f)" % (knn_scores.mean(), scores.std() * 2))
+print("KNN F1 Score: %0.2f (+/- %0.2f)" % (knn_scores.mean(), knn_scores.std() * 2))
 
 
 
