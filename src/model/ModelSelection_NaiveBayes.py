fea85033117f94366d94e7265a4cbf4032413b62
yOoMarvin <marvin.messenzehl@gmail.com>
2017-11-24 20:11:33 +0100

    Changes for naive bayes

--- src/model/ModelSelection_NaiveBayes.py
+++ src/model/ModelSelection_NaiveBayes.py
@@ -12,13 +12,46 @@ data = pd.read_csv("../../data/processed/train_set.csv", index_col=0)
 df = pd.DataFrame(data)
 
 # Build Classifier object with DataFrame and column name of truth values
-c = ct.Classifier(df,"productivity_binned")
+c = ct.Classifier(df,"productivity_binned_binary")
 
-# drop columns not needed for Classification
+### drop single columns not needed for Classification
 c.dropColumns([
-        "original_title",
-        "id.1"
+        "original_title"
+        #,"adult"
+        #,"belongs_to_collection"
+        #,"budget"
+        #,"runtime"
+        ,"year"
+        #,"quarter"
+        ,"productivity_binned_multi"
+        #,"productivity_binned_binary"
 ])
+
+### scale something if needed
+#c.scale([
+#        "budget"
+#])
+
+### drop columns by prefix if needed
+c.dropColumnByPrefix("actor")
+c.dropColumnByPrefix("director")
+c.dropColumnByPrefix("company")
+c.dropColumnByPrefix("country")
+c.dropColumnByPrefix("genre")
+c.dropColumnByPrefix("quarter_")
+
+# lets print all non-zero columns of a movie to doublecheck
+df = c.data.loc[19898]
+df = df.iloc[df.nonzero()[0]]
+print(df)
+print(c.data.columns)
+
+
+
+# get information about the data
+c.balanceInfo()
+
+
 
 # get parameters for GridSearch
 scorer = c.f1(average="micro") # use F1 score with micro averaging
