6fc0e2338eea14e6bc3f1d37d9c0940e7651a184
Adrian Kochsiek <adrian.kochsiek@outlook.de>
2017-11-26 15:30:12 +0100

    some modification on tree and svc

--- src/model/ModelSelection_decisionTree.py
+++ src/model/ModelSelection_decisionTree.py
@@ -9,13 +9,39 @@ data = pd.read_csv("../../data/processed/train_set.csv", index_col=0)
 df = pd.DataFrame(data)
 
 # Build Classifier object with DataFrame and column name of truth values
-c = ct.Classifier(df,"productivity_binned")
+c = ct.Classifier(df,"productivity_binned_binary")
 
 # drop columns not needed for Classification
 c.dropColumns([
-        "original_title",
-        "id.1"
+        "original_title"
+        #,"adult"
+        #,"belongs_to_collection"
+        #,"budget"
+        #,"runtime"
+        ,"year"
+        #,"quarter"
+        ,"productivity_binned_multi"
+        #,"productivity_binned_binary"
 ])
+
+### scale something if needed
+#c.scale([
+#        "budget"
+#])
+
+### drop columns by prefix if needed
+#c.dropColumnByPrefix("actor")
+#c.dropColumnByPrefix("director")
+#c.dropColumnByPrefix("company")
+#c.dropColumnByPrefix("country")
+#c.dropColumnByPrefix("genre")
+c.dropColumnByPrefix("quarter_")
+
+# lets print all non-zero columns of a movie to doublecheck
+df = c.data.loc[19898]
+df = df.iloc[df.nonzero()[0]]
+print(df)
+print(c.data.columns)
 
 
 # get parameters for GridSearch
@@ -29,7 +55,8 @@ cv = c.fold(
 parameters = {
     'criterion':['gini', 'entropy'],
     'max_depth':[1, 2, 3, 4, 5, 10, 50, 100, None],
-    'min_samples_split' :[2,3,4,5]
+    'min_samples_split' :[2,3,4,5],
+    'class_weight': [{'yes':1, 'no':5}]
     # parameter can be used to tweak parallel computation / n = # of jobs
     #,"n_jobs":[1]
 }
@@ -50,3 +77,10 @@ c.gridSearchBestScore(gs)
 
 # save all results in csv
 c.gridSearchResults2CSV(gs,parameters,"tree_results.csv")
+
+c.splitData()
+
+c.fit_predict(gs.best_estimator_)
+print(c.confusion_matrix())
+
+c.classification_report()
