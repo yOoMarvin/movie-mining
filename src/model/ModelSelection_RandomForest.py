74120fe88f3a505f632d325d0ddb3ef2c6e32a37
Daniel <enticr@gmx.de>
2017-11-26 18:35:16 +0100

    Adding log function of best GridSearch result

--- src/model/ModelSelection_RandomForest.py
+++ src/model/ModelSelection_RandomForest.py
@@ -8,14 +8,15 @@ Created on Thu Nov 23 10:51:44 2017
 import pandas as pd
 import ClassifierTemplate as ct
 from time import time
+import datetime
 from sklearn.ensemble import RandomForestClassifier
 import pickle
 
-def RandomForest(df):
+def RandomForest(df, f1_score_avg_method):
     classifier = ct.Classifier(df, 'productivity_binned_binary')
     # get parameters for GridSearch
     # F1 Score with micro averaging
-    score = classifier.f1(average='micro')
+    score = classifier.f1(average=f1_score_avg_method) # macro = für jede klasse Durchschnitt der Berechnung der Scores
     # Classifier is Random Forest from sklearn
     estimator = classifier.randomForest()
     # use 10 fold cross validation as for other classifiers
@@ -33,8 +34,8 @@ def RandomForest(df):
     param_grid_t = {"n_estimators": [20], # nr. of trees
                   "max_depth": [None], #max-Depth of trees. "None" means expansion of leaves until leaves are pure or until all leaves contain less than min_samples_split sample 
                   "max_features": [20], # nr. of features when looking for best split using measurement method defined in criterion
-                  "min_samples_split": [10], #minimum # of samples required to split an internal node
-                  "min_samples_leaf": [3], #minimum # of samples required to be at a leaf node
+                  "min_samples_split": [3], #minimum # of samples required to split an internal node
+                  "min_samples_leaf": [3,5], #minimum # of samples required to be at a leaf node
                   "bootstrap": [False],
                   "criterion": ["entropy"]} # measuring method for best split for nodes
     
@@ -44,30 +45,35 @@ def RandomForest(df):
     #store GridSearch result in variable gs
     gs = classifier.gridSearch(estimator,
                                score,
-                               param_grid_t,
+                               param_grid,
                                print_results=False,
                                verbose=2,
                                cv=cv)
     #Print best result of GridSearch
     classifier.gridSearchBestScore(gs)
     #save the best result
-    #save_model(gs.best_params_, gs.best_score_)
-
-    print("GridSearch for RandomForest took {} seconds".format(time()-start))
+    save_model(gs.best_params_, gs.best_score_, f1_score_avg_method)
+    
+    print(status+"GridSearch for RandomForest took {} seconds".format("%.2f" % (float(time()-start))))
     #store to csv
     classifier.gridSearchResults2CSV(gs, param_grid, "../../data/processed/randForest2.csv")
-    print(status + 'CSV with scores saved')
+    print(status + 'CSV with scores saved successfully')
     return
 
-def save_model(gs_params, gs_score):
+def save_model(gs_params, gs_score, f1_score_avg_method):
     filename = "ModelSelection_RandomForest_BestScore"
     #pickle.dump(modelRF, open(filename, "wb"))
     name = filename+".txt"
-    file = open(name,"w")
-    #file.write("Best Score: "+str(modelRF))
-    file.write("Best Score: "+str(gs_score)+" | "+str(gs_params))
-    print(status + 'Best Score and Params saved to txt File')
-    file.close() 
+    #file = open(name,"w")
+    try:
+        with open(name, 'a') as file:
+            #file.write("Best Score: "+str(modelRF))
+            gs_score = "%.4f" % float(gs_score)
+            file.write(str(datetime.datetime.now())+" | Score: "+str(gs_score)+" | F1 Average: "+f1_score_avg_method+" | "+str(gs_params)+"\n")   
+            print(status + 'Best Score and Params saved to .txt file successfully')
+    except ValueError:
+        print("Oops!  File does not exist. Failed to write to csv")
+    file.close()
     return
 
 
@@ -83,7 +89,9 @@ df = df.drop(["original_title",
             "productivity_binned_multi"],
             #"production_companies"],
             1)
-RandomForest(df)
+
+f1_score_avg_method='macro'
+RandomForest(df, f1_score_avg_method)
 
 """
 #Best score up to now:
