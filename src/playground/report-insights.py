11611b935969465ede2232866d832b50f8525c62
yOoMarvin <marvin.messenzehl@gmail.com>
2017-11-28 13:55:36 +0100

    Added serch for zero datasets

--- src/playground/report-insights.py
+++ src/playground/report-insights.py
@@ -6,6 +6,15 @@ df.head()
 
 df.describe()
 
+#Count zero values of budget and revenue
+zeros = df.query('revenue == 0 & budget == 0')
+zeros
+not_zero = df.query('revenue > 0 & budget > 0')
+not_zero
+
+relevant = df.query('revenue > 10000 & budget > 10000')
+relevant
+
 # Mean budget and revenue
 df['budget'].mean()
 df['revenue'].mean()
@@ -19,7 +28,16 @@ plt.show()
 
 
 # Scatter Revenue / Budget
-plt.scatter(df['budget'], df['revenue'])
+plt.figure(figsize=(5,5))
+
+plt.scatter(relevant['budget'], relevant['revenue'])
+plt.plot([0, 1], [0, 1], linestyle='--', lw=2, color='r', label='productivity 1', alpha=.8) # draw diagonal
+
 plt.xlabel("Budget")
 plt.ylabel("Revenue")
+plt.legend()
 plt.show()
+
+# Productivity
+prd = not_zero['revenue'] / not_zero['budget']
+prd
