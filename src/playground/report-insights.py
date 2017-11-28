5b8f07b2770ff22bb2c42a31f17ee01d9da57999
yOoMarvin <marvin.messenzehl@gmail.com>
2017-11-28 09:56:56 +0100

    Added some other explorations and basics sums

--- src/playground/report-insights.py
+++ src/playground/report-insights.py
@@ -6,7 +6,20 @@ df.head()
 
 df.describe()
 
+# Mean budget and revenue
+df['budget'].mean()
+df['revenue'].mean()
+
+# Mean productivity rate
+print(df['revenue'].mean() / df['budget'].mean())
 
 # Plotting budget and revenue
 df['revenue'].plot(kind='hist')
+plt.show()
+
+
+# Scatter Revenue / Budget
+plt.scatter(df['budget'], df['revenue'])
+plt.xlabel("Budget")
+plt.ylabel("Revenue")
 plt.show()
